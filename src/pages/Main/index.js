7ba329db3c829bd08153b81f3d035c8cbef976db
indraseptama <indra.septama@ecomindo.com>
2021-12-29 22:37:02 +0700

    feat: change styling main game

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,19 @@
-import { Box, Flex, Image, Spinner, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Image,
+  Spinner,
+  Text,
+  VStack,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import Puzzle from "./components/Puzzle";
 import { nahtuhClient } from "nahtuh-client";
@@ -16,10 +31,18 @@ const Main = ({ isHost, nickName }) => {
   const [groups, setGroups] = useState([]);
   const [level, setLevel] = useState(1);
   const [solution, setSolution] = useState([]);
+  const [members, setMembers] = useState([]);
 
   useEffect(() => {
     getParticipant();
   }, []);
+
+  useEffect(() => {
+    if (groups.length > 0 && groupName !== "") {
+      const group = groups.find((item) => item.name === groupName);
+      if (group) setMembers(group.members);
+    }
+  }, [groups, groupName]);
 
   useEffect(() => {
     nahtuhClient.onIncomingMessage = onIncomingMessage;
@@ -167,31 +190,30 @@ const Main = ({ isHost, nickName }) => {
       width={"100%"}
       px={"40px"}
       minHeight={"100vh"}
-      background={"linear-gradient(180deg, #FFE4C6 2.16%, #EC9B3E 132.04%)"}
+      background={"#262d53"}
     >
-      <Flex py="40px" flex={1}>
-        <Flex
-          bg={"white"}
-          borderRadius={"16px"}
-          width={"100%"}
-          height={"max-content"}
-          p="16px"
-          direction={"column"}
-          alignItems={"center"}
-        >
-          <Text mb="16px">Solution</Text>
-          <Image src={imageUrl} boxSize={"200px"} />
-        </Flex>
+      <Flex
+        bg={"rgba(0, 0, 0, 0.5)"}
+        direction={"column"}
+        flex={1}
+        my="40px"
+        p="8px"
+        borderRadius={"16px"}
+        height={"max-content"}
+      >
+        <Text color={"white"}>Team Member</Text>
+        {members.map((item) => (
+          <Text color={"white"}>{item.participantName}</Text>
+        ))}
       </Flex>
       <Flex
         overflow={"hidden"}
         justifyContent={"center"}
         alignItems={"center"}
-        direction={"column"}
         flex={2}
       >
         <Box>
-          <Text textAlign={"center"} mb={"16px"}>
+          <Text textAlign={"center"} mb={"8px"} color={"white"}>
             {`Solve the puzzle ${nickName}!`}
           </Text>
           <Puzzle
@@ -205,42 +227,155 @@ const Main = ({ isHost, nickName }) => {
       </Flex>
 
       <Flex
+        bg={"rgba(0, 0, 0, 0.5)"}
         direction={"column"}
         flex={1}
-        py="40px"
-        maxHeight={"100vh"}
-        overflow={"hidden"}
+        my="40px"
+        p="8px"
+        borderRadius={"16px"}
+        maxHeight={"calc(100vh - 80px);"}
       >
-        <VStack
+        <Flex
+          direction={"column"}
+          alignItems={"center"}
           p="16px"
-          bg={"white"}
+          bg={"rgba(0, 0, 0, 0.2)"}
           borderRadius={"16px"}
-          overflowY={"scroll"}
         >
-          <Text textAlign={"center"} mb="16px">
+          <Text mb="8px" color={"white"}>
+            Solution
+          </Text>
+          <Image src={imageUrl} boxSize={"70px"} />
+        </Flex>
+        <Box height={"10px"} />
+        <Flex
+          direction={"column"}
+          p="16px"
+          bg={"rgba(0, 0, 0, 0.2)"}
+          borderRadius={"16px"}
+          flex={"auto"}
+          height={"100%"}
+        >
+          <Text textAlign={"center"} mb="16px" color={"white"}>
             Leaderboard
           </Text>
-          {groups.map((item, i) => (
-            <Flex
-              p="12px"
+          <Box
+            flex={1}
+            height={"100%"}
+            overflowY={"scroll"}
+            css={{
+              "&::-webkit-scrollbar": {
+                width: "4px",
+              },
+              "&::-webkit-scrollbar-track": {
+                width: "6px",
+              },
+              "&::-webkit-scrollbar-thumb": {
+                background: "grey",
+                borderRadius: "24px",
+              },
+            }}
+          >
+            <Table
               width={"100%"}
-              bg={"linear-gradient(96.18deg, #479FF4 3.21%, #1F43C1 105.94%)"}
-              textAlign={"left"}
-              borderRadius={"16px"}
-              alignItems={"center"}
-              justifyContent={"space-between"}
+              maxHeight={"100%"}
+              flex={1}
+              variant={"unstyled"}
             >
-              <Text>
-                {i + 1} {item.name}
-              </Text>
-              {item.duration ? (
-                <Text>{item.duration / 1000}s</Text>
-              ) : (
-                <Text>0</Text>
-              )}
-            </Flex>
-          ))}
-        </VStack>
+              <Thead>
+                <Tr>
+                  <Th pl="0px" pr="7px" py={"12px"}>
+                    <Text
+                      borderRadius={"8px"}
+                      py="6px"
+                      px="11px"
+                      textAlign={"center"}
+                      border="1px solid #FFFFFF"
+                      color={"white"}
+                      fontSize={"10px"}
+                      textTransform={"none"}
+                    >
+                      Rank
+                    </Text>
+                  </Th>
+                  <Th pl="0px" pr="7px" py={"12px"}>
+                    <Text
+                      borderRadius={"8px"}
+                      py="6px"
+                      px="11px"
+                      textAlign={"center"}
+                      border="1px solid #FFFFFF"
+                      color={"white"}
+                      fontSize={"10px"}
+                      textTransform={"none"}
+                    >
+                      Group Name
+                    </Text>
+                  </Th>
+                  <Th isNumeric px="0px" py={"12px"}>
+                    <Text
+                      borderRadius={"8px"}
+                      py="6px"
+                      px="11px"
+                      textAlign={"center"}
+                      border="1px solid #FFFFFF"
+                      color={"white"}
+                      fontSize={"10px"}
+                      textTransform={"none"}
+                    >
+                      Duration
+                    </Text>
+                  </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {groups.map((item, index) => (
+                  <Tr
+                    width={"100%"}
+                    bg={item.name === groupName ? "#FDC448" : "transparent"}
+                    color={item.name === groupName ? "#231B58" : "white"}
+                    fontWeight={item.name === groupName ? "500" : "400"}
+                  >
+                    <Td
+                      textAlign={"center"}
+                      isNumeric={true}
+                      borderLeftRadius={"4px"}
+                      pl="0px"
+                      pr="7px"
+                      py={"4px"}
+                      fontSize={"14px"}
+                    >
+                      {index + 1}
+                    </Td>
+                    <Td
+                      textAlign={"center"}
+                      pl="0px"
+                      pr="7px"
+                      py={"4px"}
+                      fontSize={"14px"}
+                    >
+                      {item.name}
+                    </Td>
+                    <Td
+                      textAlign={"right"}
+                      borderRightRadius={"4px"}
+                      pr="7px"
+                      pl="0px"
+                      py={"4px"}
+                      fontSize={"14px"}
+                    >
+                      {item.duration ? (
+                        <Text>{item.duration / 1000}s</Text>
+                      ) : (
+                        <Text>0</Text>
+                      )}
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </Box>
+        </Flex>
       </Flex>
     </Flex>
   );
